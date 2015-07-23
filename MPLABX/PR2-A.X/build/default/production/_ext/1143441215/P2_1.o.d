983d21d58e3aca7db8e65d8048c4197e2b401b7f
CytronTrainee <cytontrainee@gmail.com>
2015-07-23 12:08:39 +0800

    DIY PR2A
    Updated version.

---
+++ MPLABX/PR2-A.X/build/default/production/_ext/1143441215/P2_1.o.d
@@ -0,0 +1,3 @@
+build/default/production/_ext/1143441215/P2_1.o:  \
+	C:/Documents\ and\ Settings/Sales/Desktop/PIC16F84A\ DIY\ PR2-A/Source\ File/P2_1.asm  \
+
--- PR2 MPLABX/build/default/production/P2_1.o.d
+++
@@ -1,4 +0,0 @@
-build/default/production/P2_1.o : \
-	C:/Users/Hatake_Chin/MPLABXProjects/PR\ 2\ _\ 7-segment\ Decoder.X/P2_1.asm \
-	C:/Program\ Files\ (x86)/Microchip/MPLABX/mpasmx/P16F84A.INC \
-
